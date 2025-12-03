e5656848968262a6e331f9b080077e9ceda964b2
Anish-Programmer <at7anish@gmail.com>
2025-12-03 16:19:44 +0545

    Task 3 completed

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 3, 8, 15, 22, 5, 12, 7, 30 };
+
+            Console.WriteLine("Even Numbers:");
+            ProcessNumbers(numbers, n => n % 2 == 0);
+
+            Console.WriteLine("\nNumbers Greater Than 10:");
+            ProcessNumbers(numbers, n => n > 10);
+
+            Console.ReadLine();
+        }
+
+        // Method that accepts array + used built-in Func delegate  
+        static void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+        {
+            foreach (int n in numbers)
+            {
+                if (condition(n))   // apply the condition
+                {
+                    Console.WriteLine(n);
+                }
+            }
         }
     }
 }
