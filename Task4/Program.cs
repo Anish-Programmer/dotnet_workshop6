30af2c3cc3ea8bd0003707e5e468fa28d8a8cc0f
Anish-Programmer <at7anish@gmail.com>
2025-12-03 16:33:52 +0545

    Task 4 completed

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // -------------------- 1. SELECT / PROJECTION --------------------
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            var squaredNumbers = numbers.Select(n => n * n).ToList(); // --using LINQ Select to square each number
+
+            Console.WriteLine("Squared Numbers:");
+            foreach (var sq in squaredNumbers)
+            {
+                Console.WriteLine(sq);
+            }
+                
+
+            // -------------------- 2. FILTERING (WHERE) --------------------
+            List<Book> books = new List<Book> //---list of book objects
+            {
+                new Book { Title="C# Basics", Price=500 },
+                new Book { Title="Advanced .NET", Price=1500 },
+                new Book { Title="Data Structures", Price=1200 },
+                new Book { Title="Story Book", Price=300 }
+            };
+
+            var premiumBooks = books.Where(b => b.Price > 1000).ToList();
+
+            Console.WriteLine("\nBooks priced above Rs.1000:");
+            foreach (var book in premiumBooks)
+                Console.WriteLine($"{book.Title} - Rs.{book.Price}");
+
+
+            // -------------------- 3. SORTING (OrderBy) --------------------
+            List<Student> students = new List<Student> //---list of student objects
+            {
+                new Student { Name="Anish" },
+                new Student { Name="Bikash" },
+                new Student { Name="Arjun" },
+                new Student { Name="Nikita" },
+                new Student { Name="Pramisha" },
+                new Student { Name="Laxman" },
+                new Student { Name="Dilip" },
+                new Student { Name="Aayush" },
+                new Student { Name="Sujan" },
+                new Student { Name="Kritika" }
+            };
+
+            var sortedStudents = students.OrderBy(s => s.Name).ToList();
+
+            Console.WriteLine("\nStudents Sorted Alphabetically for AAA:");
+            foreach (var s in sortedStudents)
+            {
+                Console.WriteLine(s.Name);
+            }
+           
+
+            Console.ReadLine();
         }
     }
+
 }
